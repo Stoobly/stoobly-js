8b65685435d7279b630ade4c85ce31a83c9117b5
jvlythical <lvlichael8@gmail.com>
2025-12-13 00:13:21 -0800

    refactor(stoobly): Replace getter properties with factory methods
    Replace cypress, interceptor, and playwright getter properties with cypressInterceptor(), interceptor(), and playwrightInterceptor() factory methods that accept InterceptorOptions.

--- src/stoobly.ts
+++ src/stoobly.ts
@@ -1,16 +1,14 @@
+import { InterceptorFramework } from '@constants/option';
 import {DEFAULT_UI_URL} from './constants/config';
 import {Cypress} from './core/cypress';
 import {ConfigResource, HttpService} from './core/http';
 import {Interceptor} from './core/interceptor';
 import {Playwright} from './core/playwright';
 import {Config} from './models/config';
+import { InterceptorOptions } from './types';
 
 export default class Stoobly {
   httpService: HttpService;
-
-  private _cypress: Cypress | null = null;
-  private _interceptor: Interceptor | null = null;
-  private _playwright: Playwright | null = null;
 
   constructor(apiUrl: string = DEFAULT_UI_URL) {
     this.httpService = new HttpService(apiUrl);
@@ -21,31 +19,27 @@ export default class Stoobly {
     return new Config(resource);
   }
 
-  get cypress() {
-    if (!this._cypress) {
-      this._cypress = new Cypress();
-    }
-
-    return this._cypress;
-  }
-
-  get interceptor() {
-    if (!this._interceptor) {
-      this._interceptor = new Interceptor();
-    }
-
-    return this._interceptor;
-  }
-
-  get playwright() {
-    if (!this._playwright) {
-      this._playwright = new Playwright();
-    }
-
-    return this._playwright;
-  }
-
   set apiUrl(url: string) {
     this.httpService = new HttpService(url);
+  }
+
+  cypressInterceptor(options: InterceptorOptions) {
+    return new Cypress(options);
+  }
+
+  interceptor(options: InterceptorOptions) {
+    if (options.framework === InterceptorFramework.CYPRESS) {
+      return new Cypress(options);
+    }
+
+    if (options.framework === InterceptorFramework.PLAYWRIGHT) {
+      return new Playwright(options);
+    }
+
+    return new Interceptor(options);
+  }
+
+  playwrightInterceptor(options: InterceptorOptions) {
+    return new Playwright(options);
   }
 }
